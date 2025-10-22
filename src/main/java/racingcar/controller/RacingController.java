ec4dec7fdd7938293ff03b4438fa2bdf752c3929
Seokwoo <seokwoo1999@naver.com>
2025-10-22 16:34:43 +0900

    feat(RacingController): run 메서드에서 util 클래스를 통해 carNames 반환 요청
    - Application: InputView, OutputView 생성자 주입 및 run() 메서드 실행

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,7 +1,10 @@
 package racingcar.controller;
 
+import racingcar.util.CarNamesParser;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
+
+import java.util.List;
 
 public class RacingController {
     private final InputView inputView;
@@ -10,5 +13,10 @@ public class RacingController {
     public RacingController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
         this.outputView = outputView;
+    }
+
+    public void run() {
+        String carNamesInput = inputView.readCarNames();
+        List<String> carNames = CarNamesParser.parse(carNamesInput);
     }
 }
