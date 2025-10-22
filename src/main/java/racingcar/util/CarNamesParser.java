fd60717422a7c61fa7fdb80b0ff06ccad06ef206
Seokwoo <seokwoo1999@naver.com>
2025-10-22 16:32:42 +0900

    feat(CarNameParser): carName을 파싱하는 util 클래스 구현
    - 빈 값(empty, null)이 입력되었는지 검증
    - 설정된 구분자 기반으로 파싱된 carNames를 반환

---
+++ src/main/java/racingcar/util/CarNamesParser.java
@@ -0,0 +1,28 @@
+package racingcar.util;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CarNamesParser {
+    private static final String DELIMITER = ",";
+
+    private CarNamesParser() {
+    }
+
+    public static List<String> parse(String input) {
+        validateNotEmpty(input);
+        return parseToList(input);
+    }
+
+    private static void validateNotEmpty(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
+        }
+    }
+
+    private static List<String> parseToList(String input) {
+        return Arrays.stream(input.split(DELIMITER))
+                .map(String::trim)
+                .toList();
+    }
+}
