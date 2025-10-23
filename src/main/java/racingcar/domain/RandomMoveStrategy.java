de2b58bc2ede75c2105cea544f1ae723ca688532
Seokwoo <seokwoo1999@naver.com>
2025-10-23 22:00:37 +0900

    style(RandomMoveStrategy): 단순 줄 맞춤

--- src/main/java/racingcar/domain/RandomMoveStrategy.java
+++ src/main/java/racingcar/domain/RandomMoveStrategy.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomMoveStrategy implements MoveStrategy {
     private final static int MIN = 0;
-    private final static int MAX= 9;
+    private final static int MAX = 9;
 
     @Override
     public int generatePower() {
