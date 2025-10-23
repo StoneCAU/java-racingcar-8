246c9b76583f12d450b401ef202b6ecf9e514239
Seokwoo <seokwoo1999@naver.com>
2025-10-23 16:56:50 +0900

    feat(OutputView): 실행 결과 메세지 출력 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,4 +1,15 @@
 package racingcar.view;
 
 public class OutputView {
+    private static final String NEW_LINE = System.lineSeparator();
+    private static final String RESULT_TITLE = "실행 결과";
+
+    public void printResultMessage() {
+        printNewLine();
+        System.out.println(RESULT_TITLE);
+    }
+
+    public void printNewLine() {
+        System.out.print(NEW_LINE);
+    }
 }
