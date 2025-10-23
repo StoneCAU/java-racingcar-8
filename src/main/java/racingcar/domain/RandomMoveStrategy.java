204c8fe9dbdb2a055a1619c42c40af74ac01a1d6
Seokwoo <seokwoo1999@naver.com>
2025-10-23 15:54:07 +0900

    feat(MoveStrategy): move 인터페이스 및 구현체 구현
    - 추 후 테스트 고려, Fix 방식 생성 예정

---
+++ src/main/java/racingcar/domain/RandomMoveStrategy.java
@@ -0,0 +1,13 @@
+package racingcar.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomMoveStrategy implements MoveStrategy {
+    private final static int MIN = 0;
+    private final static int MAX= 9;
+
+    @Override
+    public int generate() {
+        return Randoms.pickNumberInRange(MIN, MAX);
+    }
+}
