8fc6edec8d4be642ffc3868150f46692488af81d
Seokwoo <seokwoo1999@naver.com>
2025-10-23 21:07:43 +0900

    feat(OutputView): 라운드 별 결과 출력 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,8 +1,14 @@
 package racingcar.view;
 
+import dto.CarStatus;
+
+import java.util.List;
+
 public class OutputView {
     private static final String NEW_LINE = System.lineSeparator();
     private static final String RESULT_TITLE = "실행 결과";
+    private static final String RESULT_FORMAT = "%s : %s";
+    private static final String POSITION_SYMBOL = "-";
 
     public void printResultMessage() {
         printNewLine();
@@ -11,5 +17,14 @@ public class OutputView {
 
     public void printNewLine() {
         System.out.print(NEW_LINE);
+    }
+
+    public void printRoundResult(List<CarStatus> statuses) {
+        statuses.forEach(status ->
+                System.out.printf(RESULT_FORMAT + NEW_LINE,
+                        status.name(),
+                        POSITION_SYMBOL.repeat(status.position()))
+        );
+        printNewLine();
     }
 }
