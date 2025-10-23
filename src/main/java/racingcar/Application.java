47f455d3a1ba22c120c1e060bb9411f6e7974a4e
Seokwoo <seokwoo1999@naver.com>
2025-10-23 17:05:38 +0900

    feat(Application): RacingController 생성자 매개변수 수정
    - MoveStrategy 추가

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,12 +1,16 @@
 package racingcar;
 
 import racingcar.controller.RacingController;
+import racingcar.domain.MoveStrategy;
+import racingcar.domain.RandomMoveStrategy;
 import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        RacingController racingController = new RacingController(new InputView(), new OutputView());
+        MoveStrategy moveStrategy = new RandomMoveStrategy();
+
+        RacingController racingController = new RacingController(new InputView(), new OutputView(), moveStrategy);
         racingController.run();
     }
 }
