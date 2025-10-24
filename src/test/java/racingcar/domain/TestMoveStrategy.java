bea8ddd006d6936365da315b6ee1c35a1815611f
Seokwoo <seokwoo1999@naver.com>
2025-10-24 15:58:17 +0900

    refactor(TestMoveStrategy): 테스트용 MoveStrategy 리팩토링
    - 각 자동차의 라운드 별 power를 입력받아 실행하는 구조로 변경

---
+++ src/test/java/racingcar/domain/TestMoveStrategy.java
@@ -0,0 +1,15 @@
+package racingcar.domain;
+
+public class TestMoveStrategy implements MoveStrategy {
+    private final int[] powers;
+    private int index = 0;
+
+    public TestMoveStrategy(int... powers) {
+        this.powers = powers;
+    }
+
+    @Override
+    public int generatePower() {
+        return powers[index++ % powers.length];
+    }
+}
